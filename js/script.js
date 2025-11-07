014c0fb022393bfb150cc11c5ac7066cdac70ea9
Wissembrahem <wbrahem19@gmail.com>
2025-11-07 17:51:24 +0100

    seconda parte dell'esercizio

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
+const titleElem = document.querySelector(".randomAppear");
+const numbers = fiveRandomNum();
+titleElem.innerHTML = numbers
+
+setTimeout(function () {
+    titleElem.innerHTML = '';
+}, 3000);
+
+
 function fiveRandomNum() {
- const number = [];
- for ( i = 0; i <= 5; i++){
-    number.push(Math.floor(Math.random()*10)+ 1);
- }
- return number;
-}   
-const RandomNumbers = fiveRandomNum();
+
+    const numbers = [];
+
+    for (i = 0; i < 5; i++) {
+        const randomNumber = Math.floor(Math.random() * 100) + 1;
+        if (!numbers.includes(randomNumber)) { // se "randomNumbers" NON è incluso in "numbers"
+            numbers.push(Math.floor(Math.random() * 100) + 1)
+        }
+    }
+
+    return numbers;
+}
+
+const RandomNumbers = numbers;
 console.log(RandomNumbers)
+
